479fb474935846f782c64f8ac655758344638737
wangzihe1998 <70945748+wangzihe1998@users.noreply.github.com>
2023-02-22 00:26:06 +0800

    Add files via upload

---
+++ Chapter2/hello_world.py
@@ -0,0 +1,11 @@
+print("Hello python world!")
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python crash course world!"
+print(message)
+
+
+mesage = "Hello Python crash course reader!"
+print(mesage)
