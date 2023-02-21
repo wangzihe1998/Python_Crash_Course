7da5444a3e417b07313dcbe39ead6d80bac42c40
wangzihe1998 <70945748+wangzihe1998@users.noreply.github.com>
2023-02-22 00:27:33 +0800

    Add files via upload

---
+++ Chapter4/dimensions.py
@@ -0,0 +1,17 @@
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+
+dimensions = (200,50)
+for dimensions in dimensions:
+    print(dimensions)
+
+dimensions = (200,50)
+print("Original dimensions")
+for dimensions in dimensions:
+    print(dimensions)
+
+dimensions = (400,100)
+print("\nModified dimensions")
+for dimensions in dimensions:
+    print(dimensions)
